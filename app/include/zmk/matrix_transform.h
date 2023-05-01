57ad7ef93f78074e7ef46110bfce7c2a71a2b672
Kimeshan Naidoo <kimeshann@soliditech.com>
2023-05-01 12:58:23 +0200

    Revert "fix(keymaps): fix keypresses that are not in the transform"
    This reverts commit 309359b3

--- app/include/zmk/matrix_transform.h
+++ app/include/zmk/matrix_transform.h
@@ -6,4 +6,4 @@
 
 #pragma once
 
-int32_t zmk_matrix_transform_row_column_to_position(uint32_t row, uint32_t column);
+uint32_t zmk_matrix_transform_row_column_to_position(uint32_t row, uint32_t column);
